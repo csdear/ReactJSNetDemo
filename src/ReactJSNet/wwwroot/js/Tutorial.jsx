0051f68dd8e635ba399c9d4123dc7ac56af5167a
csdear <csdear138@gmail.com>
2019-01-29 22:36:36 -0600

    MD HTML markup problem, to render... not display... use dangerouslySetHTML

--- src/ReactJSNet/wwwroot/js/Tutorial.jsx
+++ src/ReactJSNet/wwwroot/js/Tutorial.jsx
@@ -17,16 +17,21 @@ class CommentList extends React.Component {
 
 
 class Comment extends React.Component {
-    render() {
+    rawMarkup() {
         const md = new Remarkable();
+        const rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    }
+    render() {
         return (
             <div className="comment">
                 <h2 className="commentAuthor">{this.props.author}</h2>
-                {md.render(this.props.children.toString())}
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
             </div>
         );
     }
 }
+
 
 
 
