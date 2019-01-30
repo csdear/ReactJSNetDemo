a2da109a17b296e7e48a7a7be13df05c9605cba4
csdear <csdear138@gmail.com>
2019-01-29 22:50:00 -0600

    passing JSON props from one parent compt to a child compt. Displaying that JSON data via a map function to iterate and render each.

--- src/ReactJSNet/wwwroot/js/Tutorial.jsx
+++ src/ReactJSNet/wwwroot/js/Tutorial.jsx
@@ -1,58 +1,57 @@
 ﻿
-class CommentList extends React.Component {
-    render() {
-        return (
-            <div className="commentList">
-                <Comment author="Daniel Lo Nigro">
-                    Hello ReactJS.NET World!
-                </Comment>
-                <Comment author="Pete Hunt">This is one comment</Comment>
-                <Comment author="Jordan Walke">
-                    This is *another* comment
-                </Comment>
-            </div>
-        );
-    }
-}
-
-
-class Comment extends React.Component {
-    rawMarkup() {
-        const md = new Remarkable();
-        const rawMarkup = md.render(this.props.children.toString());
-        return { __html: rawMarkup };
-    }
-    render() {
-        return (
-            <div className="comment">
-                <h2 className="commentAuthor">{this.props.author}</h2>
-                <span dangerouslySetInnerHTML={this.rawMarkup()} />
-            </div>
-        );
-    }
-}
-
-
-
-
-class CommentForm extends React.Component {
-   render() {
-       return (
-           <div className="commentForm">Hello, world! I am a CommentForm.</div>
-       );
-   }
-}
+const data = [
+    { Id: 1, Author: 'Daniel Lo Nigro', Text: 'Hello ReactJS.NET World!' },
+    { Id: 2, Author: 'Pete Hunt', Text: 'This is one comment' },
+    { Id: 3, Author: 'Jordan Walke', Text: 'This is *another* comment' },
+];
 
 class CommentBox extends React.Component {
     render() {
         return (
             <div className="commentBox">
 					<h1>Comments</h1>
-						<CommentList />
+						<CommentList data={this.props.data} />
 						<CommentForm />
 			</div>
         );
     }
 }
 
-ReactDOM.render(<CommentBox />, document.getElementById('content'));
+ReactDOM.render(<CommentBox data={data}  />, document.getElementById('content'));
+
+		class CommentList extends React.Component {
+    render() {
+        const commentNodes = this.props.data.map(comment => (
+            <Comment author={comment.Author} key={comment.Id}>
+                {comment.Text}
+            </Comment>
+        ));
+        return <div className="commentList">{commentNodes}</div>;
+    }
+}
+
+
+				class Comment extends React.Component {
+					rawMarkup() {
+						const md = new Remarkable();
+						const rawMarkup = md.render(this.props.children.toString());
+						return { __html: rawMarkup };
+					}
+					render() {
+						return (
+							<div className="comment">
+								<h2 className="commentAuthor">{this.props.author}</h2>
+								<span dangerouslySetInnerHTML={this.rawMarkup()} />
+							</div>
+						);
+					}
+				}
+
+		class CommentForm extends React.Component {
+		   render() {
+			   return (
+				   <div className="commentForm">Hello, world! I am a CommentForm.</div>
+			   );
+		   }
+		}
+
